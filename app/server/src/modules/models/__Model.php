e2231ddcdc4dd3aa691a29ff2dc028f26102ab4d
TimurDev <dev.schuchkin.timur@gmail.com>
2025-04-13 20:28:14 +0300

    finished models

--- app/server/src/modules/models/__Model.php
+++ app/server/src/modules/models/__Model.php
@@ -1,34 +1,30 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Models;
 
 abstract class __Model
 {
-	protected function prepareData(array &$data): bool
+	protected function validateId(array $data): bool
 	{
 		if (empty($data)) return false;
 
-		foreach ($data as &$item) {
-			$item = trim($item);
-			if (empty($item)) return false;
-			$item = htmlspecialchars($item);
+		foreach ($data as $id) {
+			if (!is_int($id) || $id <= 0) return false;
 		}
 
 		return true;
 	}
 
-	protected function validateId(int $id): bool
+	protected function validateText(array &$data): bool
 	{
-		if (empty($id)) return false;
-		elseif (!is_numeric($id)) return false;
-		else return true;
-	}
+		if (empty($data)) return false;
 
-	private function validateText(string $text, int $min, int $max): bool
-	{
-		if (empty($text)) return false;
-		elseif (!is_string($text)) return false;
-		elseif (strlen($text) < $min || strlen($text) > $max) return false;
-		else return true;
+		foreach ($data as &$string) {
+			if (!is_string($string) && !is_numeric($string)) return false;
+			$string = trim((string) $string);
+			if ($string === '') return false;
+		}
+
+		return true;
 	}
 }
