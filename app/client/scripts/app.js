7b14306a95d91a4887efa3e606a7c3a87910f6e5
TimurDev <dev.schuchkin.timur@gmail.com>
2025-04-14 20:31:42 +0300

    updated scripts

--- app/client/scripts/app.js
+++ app/client/scripts/app.js
@@ -3,4 +3,20 @@ import { Http } from "./http.js";
 
 const root = document.getElementById('root');
 
-root.innerHTML = Html.basicHeader();
+function renderLogin() {
+	root.innerHTML = Html.basicHeader() + Html.logForm() + Html.footer();
+}
+
+function renderRegister() {
+	root.innerHTML = Html.basicHeader() + Html.regForm() + Html.footer();
+}
+
+renderLogin();
+
+root.addEventListener('click', (event) => {
+	if (event.target.id === 'regBtn') {
+		renderRegister();
+	} else if (event.target.id === 'logBtn') {
+		renderLogin();
+	}
+});
--- app/client/scripts/html.js
+++ app/client/scripts/html.js
@@ -8,10 +8,10 @@ export class Html {
 				<div>
 					<ul class="flex">
 						<li>
-							<button class="inline-block px-6 py-2 bg-blue-600 transition duration-250 hover:bg-blue-800 text-3xl text-white font-medium rounded-lg mx-4">Login</button>
+							<button id="logBtn" class="inline-block px-6 py-2 bg-blue-600 transition duration-250 hover:bg-blue-800 text-3xl text-white font-medium rounded-lg mx-4">Login</button>
 						</li>
 						<li>
-							<button class="inline-block px-6 py-2 bg-yellow-300 transition duration-250 hover:bg-yellow-600 text-3xl text-black font-medium rounded-lg mx-4">Registration</button>
+							<button id="regBtn" class="inline-block px-6 py-2 bg-yellow-300 transition duration-250 hover:bg-yellow-600 text-3xl text-black font-medium rounded-lg mx-4">Registration</button>
 						</li>
 					</ul>
 				</div>
@@ -24,15 +24,49 @@ export class Html {
 	}
 
 	static footer() {
-		return ``;
+		return `
+			<footer class="w-full bg-zinc-700 my-6 rounded-2xl px-12 py-1 flex flex-[0_0_auto]">
+				<div class="flex justify-between w-full items-center">
+					<span class="inline-block">
+						<a href="https://github.com/TimurDev-hub" title="GitHub" target="_blank" class="inline-block w-16 h-16 mr-1 bg-[url(/app/client/imgs/github.svg)] bg-cover"></a>
+						<a href="https://x.com" title="Twitter" target="_blank" class="inline-block w-16 h-16 bg-[url(/app/client/imgs/twitter.svg)] bg-cover"></a>
+					</span>
+					<p title="Creator" class="inline-block mx-4 text-3xl text-white font-medium">Created by Timur &copy;</p>
+				</div>
+			</footer>
+		`;
 	}
 
 	static regForm() {
-		return ``;
+		return `
+			<div class="bg-blue-300 w-fit rounded-md mt-20 mx-auto" id="logForm">
+				<form autocomplete="on" accept-charset="UTF-8">
+					<fieldset class="flex flex-col" name="registrationFieldset">
+						<legend class="text-white mx-auto inline-block h-fit bg-blue-700 rounded-t-md w-full text-center text-5xl font-medium p-4 mb-8">Registration</legend>
+						<input class="bg-white mx-12 rounded-md px-4 py-2 w-100 text-3xl mb-6 outline-none" type="text" name="user_name" placeholder="Your username..." maxlength="20">
+						<input class="bg-white mx-12 rounded-md px-4 py-2 w-100 text-3xl mb-10 outline-none" type="password" name="user_password" placeholder="Your password..." maxlength="20">
+						<input class="bg-green-500 transition duration-250 hover:bg-green-600 rounded-md p-2 text-4xl font-medium w-80 mx-auto mb-10" type="submit" value="Create">
+					</fieldset>
+				</form>
+			</div>
+			<div class="flex flex-[1_0_auto]"></div>
+		`;
 	}
 
 	static logForm() {
-		return ``;
+		return `
+			<div class="bg-blue-300 w-fit rounded-md mt-20 mx-auto" id="logForm">
+				<form autocomplete="on" accept-charset="UTF-8">
+					<fieldset class="flex flex-col" name="logingFieldset">
+						<legend class="text-white mx-auto inline-block h-fit bg-blue-700 rounded-t-md w-full text-center text-5xl font-medium p-4 mb-8">Log in account</legend>
+						<input class="bg-white mx-12 rounded-md px-4 py-2 w-100 text-3xl mb-6 outline-none" type="text" name="user_name" placeholder="Your username..." maxlength="20">
+						<input class="bg-white mx-12 rounded-md px-4 py-2 w-100 text-3xl mb-10 outline-none" type="password" name="user_password" placeholder="Your password..." maxlength="20">
+						<input class="bg-green-500 transition duration-250 hover:bg-green-600 rounded-md p-2 text-4xl font-medium w-80 mx-auto mb-10" type="submit" value="Enter">
+					</fieldset>
+				</form>
+			</div>
+			<div class="flex flex-[1_0_auto]"></div>
+		`;
 	}
 
 	static createArea() {
