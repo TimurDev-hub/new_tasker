a530de46eee23b248eae90e7528f872c38b86a36
TimurDev <dev.schuchkin.timur@gmail.com>
2025-04-24 19:23:44 +0300

    updated Logger.php

--- app/server/src/utils/Logger.php
+++ app/server/src/utils/Logger.php
@@ -5,17 +5,18 @@ namespace Utils;
 final class Logger
 {
 	public const DB_FILE = __DIR__ . '/../core/logs/db.log';
+	public const ROUTER_FILE = __DIR__ . '/../core/logs/router.log';
 	public const TASK_FILE = __DIR__ . '/../core/logs/task.log';
 	public const USER_FILE = __DIR__ . '/../core/logs/user.log';
 
 	private static function setPrefix(): string
 	{
-		return date('Y-m-d H:i:s') . ' SERVER ERROR: ';
+		return date('Y-m-d H:i:s') . " SERVER ERROR:\n";
 	}
 
 	private static function getLog(\Throwable $exc): string
 	{
-		return 'M => ' . $exc->getMessage() . '; F=> ' . $exc->getFile() . '; L=> ' . $exc->getLine();
+		return 'M => ' . $exc->getMessage() . ";\nF=> " . $exc->getFile() . ";\nL=> " . $exc->getLine() . ";\nT=>" . $exc->getTraceAsString();
 	}
 
 	public static function handleError(\Throwable $exc, string $file): void
