a698976cc57aaf6d7e9c6a11dc4b3ee44190afe2
TimurDev <dev.schuchkin.timur@gmail.com>
2025-04-24 19:22:22 +0300

    finished index.php

--- app/server/public/index.php
+++ app/server/public/index.php
@@ -1,28 +1,31 @@
 <?php declare(strict_types=1);
 
-use Router\Router;
-
 require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use Router\Router;
 
 $uri = $_SERVER['REQUEST_URI'];
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
-$router = new Router;
-
-require_once __DIR__ . '/../src/core/router/Routes.php';
+$router = new Router();
 
 try {
+	require_once __DIR__ . '/../src/core/router/Routes.php';
+
 	if (strpos($uri, '/api') !== false && strpos($uri, '/api') === 0) $router->dispatchRoutes(uri: $uri, httpMethod: $httpMethod);
 	else require_once __DIR__ . '/../../client/view/index.html';
 
 } catch (\Throwable $exc) {
 	require_once __DIR__ . '/../../client/view/index.html';
+	http_response_code(500);
 	echo json_encode([
 		'reload' => false,
 		'error' => $exc->getMessage()
 	]);
 }
-
+/*
 echo '<pre style="color: white;">';
 $router->dumpObject();
+$router->dumpRoute('POST', '/api/auth');
 echo '</pre>';
+*/
