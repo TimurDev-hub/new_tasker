0b4cc4a027ee61a9da41bd8f99b97bf6567ae258
TimurDev <dev.schuchkin.timur@gmail.com>
2025-05-19 13:42:49 +0300

    rewrited Database class using singltone

--- app/server/src/core/db/Database.php
+++ app/server/src/core/db/Database.php
@@ -6,15 +6,25 @@ use Utils\{Parser, Logger};
 
 final class Database
 {
-	private static \PDO $pdo;
+	private static ?\PDO $pdo = null;
+
+	private function __construct() {}
+	private function __clone() {}
 
 	public static function connection(): \PDO
 	{
-		['DB_HOST' => $host, 'DB_PORT' => $port, 'DB_NAME' => $db, 'DB_USER' => $user, 'DB_PASS' => $pass] = Parser::loadEnv();
-
 		try {
-			self::$pdo = new \PDO(dsn: "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$pass");
-			self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+			if (self::$pdo === null) {
+				['DB_HOST' => $host,
+				'DB_PORT' => $port,
+				'DB_NAME' => $db,
+				'DB_USER' => $user,
+				'DB_PASS' => $pass] = Parser::get()->loadEnv();
+
+				self::$pdo = new \PDO(dsn: "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$pass");
+				self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+			}
+
 			return self::$pdo;
 
 		} catch (\PDOException $exc) {
